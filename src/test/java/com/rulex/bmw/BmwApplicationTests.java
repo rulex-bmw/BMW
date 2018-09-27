c8a4e92842b1e837cbae3e973278f1f16db3aa5c
rulex-bmw <github@rulex.tech>
2018-09-27 15:30:09 +0800

    levelDb-set  2018/9/27

--- src/test/java/com/rulex/bmw/BmwApplicationTests.java
+++ src/test/java/com/rulex/bmw/BmwApplicationTests.java
@@ -32,7 +32,6 @@ public class BmwApplicationTests {
         ByteString param = ByteString.copyFrom(bytes("1b4323334fce19d6b804eff54325747ada4eaa22f1d49c01e52ddb7875b4b6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"));
         DataBean.Data build = DataBean.Data.newBuilder().setFlag(flag).setParam(param).build();
         bsbService.producer(build);
-
         //查询数据
         Options options = new Options();
         options.createIfMissing(true);
