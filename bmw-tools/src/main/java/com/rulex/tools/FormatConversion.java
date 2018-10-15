a531ff2610bfae1f08f55fb7a17aa9f28f40188f
jianfeng <826829917@qq.com>
2018-10-15 16:53:02 +0800

    格式转化完成

--- bmw-tools/src/main/java/com/rulex/tools/FormatConversion.java
+++ bmw-tools/src/main/java/com/rulex/tools/FormatConversion.java
@@ -7,25 +7,26 @@ import java.io.IOException;
 
 public class FormatConversion {
 
-//    public static String proto_CMD = "protoc.exe -I=. --java_out=./target/classes ./target/classes/Pojo.proto";
-public static String proto_CMD = "protoc.exe -I=. --java_out=./target/classes "+PathSet.xmlPath+"pojo.proto";
+    public static String path1 = "com/rulex/bmw/pojo";
 
+    public static String path2 = "bmw-tools/target/classes/entity";
 
-    public static String javaSource = FormatConversion.class.getClass().getResource("/").getPath() + "Pojo.java";
+    public static String proto_CMD = "protoc.exe -I=. --java_out=./" + path2 + " ./" + path2 + "/pojo.proto";
 
-    public static String classOut = FormatConversion.class.getClass().getResource("/").getPath();
+    public static String javaSource = PathSet.xmlPath + path1 + "/RulexBean.java";
 
-    public static String jar_CMD = "jar cvf Pojo.jar -C target/classes/ Pojo.class";
+    public static String classOut = PathSet.xmlPath;
+
+    public static String jar_CMD = "jar cvf bean.jar -C " + path2 + "/" + path1 + " RulexBean.class";
 
 
     public static void formatConversion() throws IOException, InterruptedException {
-        System.out.println(proto_CMD);
+
         //将.proto文件转化成.java文件
         ExecuteCmdUtil.executeCmd(proto_CMD);
-        System.out.println(classOut);
+
         //将.java文件转化成.class文件
         if (JavaCompilerUtil.CompilerJavaFile(javaSource, classOut)) {
-
             System.out.println("Compiler successfully");
         } else {
             System.out.println("Compiler failure");
@@ -33,19 +34,11 @@ public static String proto_CMD = "protoc.exe -I=. --java_out=./target/classes "+
 
         //将.class文件打成jar包
         ExecuteCmdUtil.executeCmd(jar_CMD);
-
     }
 
 
     public static void main(String[] args) {
-//        try {
-//            System.out.println(ExecuteCmdUtil.executeCmd("cmd.exe /c dir"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        System.out.println(PathSet.xmlPath);
+
         try {
             formatConversion();
         } catch (IOException e) {
@@ -54,5 +47,4 @@ public static String proto_CMD = "protoc.exe -I=. --java_out=./target/classes "+
             e.printStackTrace();
         }
     }
-
-    }
+}
