59369eb6c5d428621ca5e93d516423fb02974b57
jianfeng <826829917@qq.com>
2018-10-16 17:38:32 +0800

    jar包去java文件

--- bmw-tools/src/main/java/com/rulex/tools/PathSet.java
+++ bmw-tools/src/main/java/com/rulex/tools/PathSet.java
@@ -4,6 +4,8 @@ public class PathSet {
 
     public static String xmlPath = XMLReader.class.getClass().getResource("/entity/").getPath();
 
+    public static String javaPath = XMLReader.class.getClass().getResource("/").getPath();
+
     public static String packagePath = "com.rulex.bsb.pojo";
 
 }
