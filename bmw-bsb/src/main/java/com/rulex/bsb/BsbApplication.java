872bd563d269112f636d99981d759e1df87b778e
rulex-bmw <github@rulex.tech>
2018-10-16 17:00:18 +0800

    tools 16点58分

--- bmw-bsb/src/main/java/com/rulex/bsb/BsbApplication.java
+++ bmw-bsb/src/main/java/com/rulex/bsb/BsbApplication.java
@@ -1,13 +1,12 @@
 package com.rulex.bsb;
 
-import com.rulex.bsb.pojo.RulexBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BsbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BsbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BsbApplication.class, args);
+    }
 }
