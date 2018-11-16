dc5670e981bde9c9aba64d9c75bae40b46116d25
jianfeng <826829917@qq.com>
2018-11-16 14:06:36 +0800

    合并代码

--- bmw-bsb/src/main/java/com/rulex/bsb/utils/TypeUtils.java
+++ bmw-bsb/src/main/java/com/rulex/bsb/utils/TypeUtils.java
@@ -124,7 +124,7 @@ public class TypeUtils {
     public static byte[] double2Bytes(double d) {
         long value = Double.doubleToRawLongBits(d);
         byte[] byteRet = new byte[8];
-        for(int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {
             byteRet[i] = (byte) ((value >> 8 * i) & 0xff);
         }
         return byteRet;
@@ -133,12 +133,19 @@ public class TypeUtils {
 
     public static double bytes2Double(byte[] arr) {
         long value = 0;
-        for(int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {
             value |= ((long) (arr[i] & 0xff)) << (8 * i);
         }
         return Double.longBitsToDouble(value);
     }
 
+
+    /**
+     * 首字母转大写
+     *
+     * @param lower
+     * @return String
+     */
     public static String InitialsLow2Up(String lower) {
         return lower.substring(0, 1).toUpperCase() + lower.substring(1);
     }
