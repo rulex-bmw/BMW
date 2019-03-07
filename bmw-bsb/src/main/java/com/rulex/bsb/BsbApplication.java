b58b086f09ab29f661a832d08c56cce5dcd63b68
rulex-bmw <github@rulex.tech>
2019-03-07 14:34:59 +0800

    Middleware service bus

--- bmw-bsb/src/main/java/com/rulex/bsb/BsbApplication.java
+++ bmw-bsb/src/main/java/com/rulex/bsb/BsbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BsbApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(BsbApplication.class, args);
     }
--- bmw-bsb/src/main/java/com/rulex/bsb/service/BSBService.java
+++ bmw-bsb/src/main/java/com/rulex/bsb/service/BSBService.java
@@ -9,7 +9,7 @@ import java.util.Base64;
 import java.util.Map;
 
 /**
- * BSB业务处理总线程
+ * BSB Total thread of business processing
  *
  * @author admin
  */
@@ -33,10 +33,10 @@ public class BSBService {
     }
 
     /**
-     * readposition:已上链数据key     writeposition：存入levelDB最新数据
-     * 从readposition中读取m并去map中寻找m+1
-     * m:已经上链，m+1:未上链
-     * 从levelDB中取出DATA,将不可变信息存入数据库
+     * readposition: Linked data key     writeposition：set levelDB new data
+     * get m from readpositionm and search m+1 from map
+     * m:finish on blockchain，m+1: unblockchain
+     * get DATA from levelDB,Storing immutable information in a database
      */
     public synchronized static Integer Consumer() {
         try {
@@ -50,7 +50,7 @@ public class BSBService {
             }
             //get readposition
             DataBean.Position readposition = LevelDBDao.getReadposition();
-            //从第m+1项上链
+            //Chain from the m+1 term
             byte[] prevHash = readposition.getDataKey().toByteArray();
             for(int i = 0; i < size; i++) {
                 byte[] currentHash = keyMap.get(Base64.getEncoder().encodeToString(prevHash));
@@ -58,7 +58,7 @@ public class BSBService {
                     break;
                 }
                 if (LevelDBDao.setStatus(currentHash)) {
-                    //调用消息通知机制，提示其他线程完成上链
+                    //Invokes the message notification mechanism to prompt other threads to complete the chain
                     prevHash = currentHash;
                 }
             }
