53d644494c00f030670e2eb9822f8ab50d99bf2e
rulex-bmw <github@rulex.tech>
2019-03-07 14:39:43 +0800

    Blockchain Service Bus

--- bmw-bsb/src/main/java/com/rulex/bsb/BsbApplication.java
+++ bmw-bsb/src/main/java/com/rulex/bsb/BsbApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BsbApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(BsbApplication.class, args);
     }
